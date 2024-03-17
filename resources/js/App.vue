6321d36d4da24501a1eeca2960f9d8704dc5457e
Peter Cipov <peter@ito.sk>
2024-03-17 23:23:16 +0100

    Product URLs in view

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -43,7 +43,9 @@
         <input type="text" v-model="search" v-on:keyup="getProducts()" class="form-control" placeholder="product name">
     </form>
     <ul>
-        <li v-for="product in products" :key="product.id">{{product.name.replace('&amp;amp;', '&').replace('&amp;quot;', '"')}}</li>
+        <li v-for="product in products" :key="product.id">
+            <a :href="product.url" target="_blank">{{product.name.replace('&amp;amp;', '&').replace('&amp;quot;', '"')}}</a>
+        </li>
     </ul>
     <small v-if="products.length==0 && search.length>2">No products found</small>
 </template>
@@ -51,7 +53,16 @@
 <style lang="scss" scoped>
     ul {
         list-style: none;
-        margin: 20px 0;
+        margin: 20px 10px;
         padding: 0;
+        li {
+            a {
+                text-decoration: none;
+                color: black;
+                &:hover {
+                    color: blue;
+                }
+            }
+        }
     }
 </style>
