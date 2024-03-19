ab77a3cb3bc39b8dc21a1782c6d979ea0c0a7d27
Peter Cipov <peter@ito.sk>
2024-03-19 21:47:00 +0100

    Filament light

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->darkMode(false)
             ->id('admin')
             ->path('admin')
             ->login()
